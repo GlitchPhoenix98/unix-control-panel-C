0b8cd19430f67b01e69eecaa83b84defc239b149
Glitch Phoenix <78001268+GlitchPhoenix98@users.noreply.github.com>
2022-05-26 18:38:38 +0000

    Updated to 0.10.0

--- ucp.c
+++ ucp.c
@@ -3,24 +3,23 @@
 int main (){
 	int choice;
 	// Program name, Required level for source code access
-	printf("Unix Control Panel 0.9.0");
+	printf("Unix Control Panel 0.10.0\n");
 	printf("\n\n1. Neofetch\n2. Htop\n3. Reboot\n4. Terminal\n");
 	printf("Type a number and hit enter: ");
 	scanf("%d", &choice);	
 	// Pick a choice
-	if (choice == 1) {
+	switch (choice) {
+		case 1:
 		system("neofetch");
-	// If it isnt equal to one
-	} else {
-	if (choice == 2)
+		break;
+		case 2:
 		system("htop");
-	} if (choice == 3) {
-	 char rebootchoice[1];
-		printf("Are you sure? y/n: \n");
-		scanf("%s", rebootchoice);
-		if (rebootchoice == 'y')
-			system("reboot");
+		break;
+		case 3:
+		system("sudo reboot");
+		break;
 	}
-	// DO NOT ADD 4 AS A CHOICE, IF THERE IS NO OPTION THE PROGRAM WILL AUTOMATICALLY EXIT
+
+
 	return 0;	
 }

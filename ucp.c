d7fdec8f3bfe0e140c17275469fa95174889b5f2
Glitch Phoenix <78001268+GlitchPhoenix98@users.noreply.github.com>
2022-05-26 19:17:52 +0000

    Update ucp.c

--- ucp.c
+++ ucp.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 int main (){
 	int choice;
-	// Program name, Required level for source code access
+	// Program name, what numbers to type, prompt
 	printf("Unix Control Panel 0.10.0\n");
 	printf("\n\n1. Neofetch\n2. Htop\n3. Reboot\n4. Terminal\n");
 	printf("Type a number and hit enter: ");
 	scanf("%d", &choice);	
 	// Pick a choice
-	//Requires neofetch and htop
+	// Requires neofetch and htop
 	switch (choice) {
 		case 1:
 		system("neofetch");

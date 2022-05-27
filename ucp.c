47bb39196aa91878502f3e46f0e9a7111a48989b
Glitch Phoenix <78001268+GlitchPhoenix98@users.noreply.github.com>
2022-05-27 01:44:57 +0000

    Add files via upload

--- ucp.c
+++ ucp.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 int main (){
 	int choice;
+	// Beginning of while loop
+	do {
 	// Program name, what numbers to type, prompt
-	printf("Unix Control Panel 0.10.0\n");
+	printf("Unix Control Panel 0.10.1\n");
 	printf("\n\n1. Neofetch\n2. Htop\n3. Reboot\n4. Terminal\n");
 	printf("Type a number and hit enter: ");
 	scanf("%d", &choice);	
@@ -14,14 +16,17 @@ int main (){
 		system("neofetch");
 		break;
 		case 2:
+		system("clear");
 		system("htop");
+		// Reset choice so it doesn't take all the ram.
+		int choice = 0;
 		break;
 		case 3:
 		// replace with what your init system uses or doas if you use that
 		system("sudo reboot");
 		break;
 	}
-
-
+	// If you picked htop, it will loop it so you can exit and use it again.
+	} while(choice == 2);
 	return 0;	
 }
